5174412fed1f5054c5b10f6188b9ffb0516185f0
Alex Luya <alex.luya@yahoo.com>
2016-12-27 09:59:12 +0800

    added ui.Table(Browser browser,String id)

--- src/com/dsc/selenium/ui/Table.java
+++ src/com/dsc/selenium/ui/Table.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import com.dsc.selenium.Browser;
+import com.dsc.selenium.ui.gwt.Widget;
 
 /**
  * @Author alex
@@ -16,11 +17,14 @@ import com.dsc.selenium.Browser;
  * @Version 1.0
  * @Since 1.0
  */
-public class Table extends UIObject
+public class Table extends Widget
 {
-	/**
-	 * @param wrapee
-	 */
+
+	public Table(Browser browser,String id)
+	{
+		super(browser,id);
+	}
+
 	public Table(Browser browser,WebElement wrapee)
 	{
 		super(browser,wrapee);
@@ -46,33 +50,33 @@ public class Table extends UIObject
 		getRow(row).mouseOver();
 	}
 
-//	public void test()
-//	{
-//
-//		WebElement Webtable = driver.findElement(By.id("TableID")); // Replace
-//																	// TableID
-//																	// with
-//																	// Actual
-//																	// Table ID
-//																	// or Xpath
-//		List<WebElement> TotalRowCount = Webtable.findElements(By.xpath("//*[@id='TableID']/tbody/tr"));
-//
-//		System.out.println("No. of Rows in the WebTable: " + TotalRowCount.size());
-//		// Now we will Iterate the Table and print the Values
-//		int RowIndex = 1;
-//		for (WebElement rowElement : TotalRowCount)
-//		{
-//			List<WebElement> TotalColumnCount = rowElement.findElements(By.xpath("td"));
-//			int ColumnIndex = 1;
-//			for (WebElement colElement : TotalColumnCount)
-//			{
-//				System.out.println("Row " + RowIndex + " Column " + ColumnIndex + " Data " + colElement.getText());
-//				ColumnIndex = ColumnIndex + 1;
-//			}
-//			RowIndex = RowIndex + 1;
-//		}
-//
-//	}
+	//	public void test()
+	//	{
+	//
+	//		WebElement Webtable = driver.findElement(By.id("TableID")); // Replace
+	//																	// TableID
+	//																	// with
+	//																	// Actual
+	//																	// Table ID
+	//																	// or Xpath
+	//		List<WebElement> TotalRowCount = Webtable.findElements(By.xpath("//*[@id='TableID']/tbody/tr"));
+	//
+	//		System.out.println("No. of Rows in the WebTable: " + TotalRowCount.size());
+	//		// Now we will Iterate the Table and print the Values
+	//		int RowIndex = 1;
+	//		for (WebElement rowElement : TotalRowCount)
+	//		{
+	//			List<WebElement> TotalColumnCount = rowElement.findElements(By.xpath("td"));
+	//			int ColumnIndex = 1;
+	//			for (WebElement colElement : TotalColumnCount)
+	//			{
+	//				System.out.println("Row " + RowIndex + " Column " + ColumnIndex + " Data " + colElement.getText());
+	//				ColumnIndex = ColumnIndex + 1;
+	//			}
+	//			RowIndex = RowIndex + 1;
+	//		}
+	//
+	//	}
 
 	public void mouseOver(int row, int cell)
 	{
