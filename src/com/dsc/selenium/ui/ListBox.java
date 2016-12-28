2193662898b14d965ecb77d55b28a31e76a18abf
Alex Luya <alex.luya@yahoo.com>
2016-12-28 11:48:29 +0800

    added methods to ListBox:selectedOption(),selectedOptionId(),selectedOptionIndex(),selectedOptionText()

--- src/com/dsc/selenium/ui/ListBox.java
+++ src/com/dsc/selenium/ui/ListBox.java
@@ -1,9 +1,12 @@
 /**
- * Copyright (c) (2016-2017),VSI and/or its affiliates.All rights
+ * Copyright (c) (2010-2013),Deep Sky Century and/or its affiliates.All rights
  * reserved.
  * DSC PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  **/
 package com.dsc.selenium.ui;
+
+import static com.dsc.selenium.util.Util.wrap;
+import static java.lang.String.format;
 
 import java.util.List;
 
@@ -11,8 +14,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import com.dsc.selenium.Browser;
+import com.google.common.collect.Lists;
 
 /**
+ * The Class ListBox.
+ *
  * @Author alex
  * @CreateTime Dec 30, 2014 9:08:50 PM
  * @Version 1.0
@@ -22,7 +28,12 @@ public class ListBox extends AbstractValidable
 {
 
 	/**
+	 * Instantiates a new list box.
+	 *
+	 * @param browser
+	 *            the browser
 	 * @param wrapee
+	 *            the wrapee
 	 */
 	public ListBox(Browser browser, WebElement wrapee)
 	{
@@ -30,30 +41,96 @@ public class ListBox extends AbstractValidable
 	}
 
 	/**
+	 * Ensure selected item is.
+	 *
 	 * @param level
+	 *            the level
+	 * @deprecated replaced by {@link #ensureSelectedOptionIs(int)}
 	 */
+	@Deprecated
 	public void ensureSelectedItemIs(int level)
 	{
-		ensureSelectedItemIs(Integer.toString(level));
+		ensureSelectedOptionIs(Integer.toString(level));
 	}
+
+	/**
+	 * Ensure selected item is.
+	 *
+	 * @param itemId
+	 *            the item id
+	 * @deprecated replaced by {@link #ensureSelectedOptionIs(String)}
+	 */
+	@Deprecated
 	public void ensureSelectedItemIs(String itemId)
 	{
-		if(!itemId.equals(selectedItemId()))
+		ensureSelectedOptionIs(itemId);
+	}
+
+	/**
+	 * Ensure selected item is.
+	 *
+	 * @param level
+	 *            the level
+	 */
+	public void ensureSelectedOptionIs(int level)
+	{
+		ensureSelectedOptionIs(Integer.toString(level));
+	}
+
+	/**
+	 * Ensure selected item is.
+	 *
+	 * @param optionId
+	 *            the item id
+	 */
+	public void ensureSelectedOptionIs(String optionId)
+	{
+		if (!optionId.equals(selectedOptionId()))
 		{
-			throw new IllegalStateException(String.format("%s's selected item id isn't '%s' but '%s'", id(), itemId, selectedItemId()));
+			throw new IllegalStateException(
+					String.format("%s's selected item id isn't '%s' but '%s'", id(), optionId, selectedOptionId()));
 		}
 	}
 
+	/**
+	 * Options.
+	 *
+	 * @return the list
+	 */
+	public List<Option> options()
+	{
+		List<Option> options = Lists.newArrayList();
+
+		for (WebElement elem : self().getOptions())
+		{
+			options.add(new Option(browser, elem));
+		}
+
+		return options;
+	}
+
+	/**
+	 * Select by id.
+	 *
+	 * @param optionId
+	 *            the option id
+	 */
 	public void selectById(int optionId)
 	{
 		selectById(Integer.toString(optionId));
 	}
 
+	/**
+	 * Select by id.
+	 *
+	 * @param optionId
+	 *            the option id
+	 */
 	public void selectById(String optionId)
 	{
 		for (int i = 0, len = options().size(); i < len; i++)
 		{
-			if (id(options().get(i)) == optionId)
+			if (options().get(i).idEquals(optionId))
 			{
 				self().selectByIndex(i);
 				break;
@@ -62,42 +139,88 @@ public class ListBox extends AbstractValidable
 	}
 
 	/**
-	 * @param birthYear
+	 * Select by value.
+	 *
+	 * @param value
+	 *            the value
 	 */
 	public void selectByValue(int value)
 	{
 		selectByValue(Integer.toString(value));
 	}
 
+	/**
+	 * Select by value.
+	 *
+	 * @param value
+	 *            the value
+	 */
 	public void selectByValue(String value)
 	{
 		self().selectByValue(value);
 	}
 
+	/**
+	 * Select by visible text.
+	 *
+	 * @param text
+	 *            the text
+	 */
 	public void selectByVisibleText(String text)
 	{
 		self().selectByVisibleText(text);
 	}
 
-
 	/**
-	 * @return
+	 * Selected option.
+	 *
+	 * @return the first selected option
 	 */
-	private List<WebElement> options()
+	public Option selectedOption()
 	{
-		return self().getOptions();
+
+		if(self().getAllSelectedOptions().isEmpty())
+		{
+			throw new IllegalStateException(wrap(format("no option selected in ListBox with id ----------%s---------", id())));
+		}
+
+		return new Option(browser, self().getFirstSelectedOption());
 	}
 
 	/**
-	 * @return
+	 * Selected option id.
+	 *
+	 * @return the id of first selected option
 	 */
-	private String selectedItemId()
+	public String selectedOptionId()
 	{
-		return id(self().getFirstSelectedOption());
+		return selectedOption().id();
 	}
 
 	/**
-	 * @return
+	 * Selected option index.
+	 *
+	 * @return the index of first selected option
+	 */
+	public int selectedOptionIndex()
+	{
+		return options().indexOf(selectedOption());
+	}
+
+	/**
+	 * Selected option text.
+	 *
+	 * @return the text of first selected opiton
+	 */
+	public String selectedOptionText()
+	{
+		return selectedOption().text();
+	}
+
+	/**
+	 * Self.
+	 *
+	 * @return the self
 	 */
 	private Select self()
 	{
